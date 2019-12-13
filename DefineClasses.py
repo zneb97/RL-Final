1a443615afe4aaa0c0a0eff59a56f8f727ac8d62
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2019-12-13 14:52:16 -0500

    Finalize stuff

--- DefineClasses.py
+++ DefineClasses.py
@@ -16,41 +16,25 @@ class DQN(nn.Module):
     def __init__(self, num_actions, device='cpu'):
         super().__init__()
         self.device = device
-        img_height = 84
-        img_width = 84
-        #Convolutions
-        self.num_kernels1 = 32
-        self.conv1 = nn.Conv2d(in_channels=4, out_channels=self.num_kernels1, kernel_size=3, stride=1, padding=1) 
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.maxpool_output_size1 = int(self.num_kernels1 *int(img_height / 2) * int(img_width / 2))
 
-        #Fully Connected Layers with max pool
-        # self.fc1 = nn.Linear(in_features=self.maxpool_output_size1, out_features=24)
-        # self.out = nn.Linear(in_features=24, out_features=num_actions)
-
-        # #Fully Connected Layers with convoltional
-        # self.fc1 = nn.Linear(in_features=self.num_kernels1*img_height*img_width, out_features=24)   
-        # self.out = nn.Linear(in_features=24, out_features=num_actions)
+        # Convolutions
+        self.num_kernels1 = 16
+        self.conv1 = nn.Conv2d(4, self.num_kernels1, kernel_size=8, stride=4, padding=1)
+        self.num_kernels2 = 32
+        self.conv2 = nn.Conv2d(self.num_kernels1, self.num_kernels2, kernel_size=4, stride=2, padding=1)
 
         # Fully Connected Layers
-        self.fc1 = nn.Linear(in_features=1*img_height*img_width, out_features=24)
-        self.out = nn.Linear(in_features=24, out_features=num_actions)
-
-
+        self.fc1 = nn.Linear(in_features=self.num_kernels2 * 10 * 10, out_features=256)
+        self.out = nn.Linear(in_features=256, out_features=num_actions)
 
     def forward(self, t):
         t = t.to(self.device)
-
-        # t = self.conv1(t)
-        # t = self.pool1(t)
-        # t= F.relu(t)
-
+        t = F.relu(self.conv1(t))
+        t = F.relu(self.conv2(t))
         t = t.flatten(start_dim=1)
         t = F.relu(self.fc1(t))
         t = self.out(t)
-        return t
-        
-       
+        return t.cpu()
 
 
 class ReplayMemory:
@@ -67,8 +51,10 @@ class ReplayMemory:
             self.memory[self.push_count] = experience
         self.push_count = (self.push_count + 1) % self.capacity
 
-    def sample(self, percentage=1):
-        return random.sample(self.memory, int(self.batch_size*percentage))
+    def sample(self, batch_size=None):
+        if batch_size is None:
+            batch_size = self.batch_size
+        return random.sample(self.memory, batch_size)
 
     def can_provide_sample(self):
         return len(self.memory) >= self.batch_size
@@ -88,18 +74,22 @@ class EpsilonGreedyStrategy:
 class Agent:
     def __init__(self, strategy, num_actions, device, memory):
         self.current_step = 0
+        self.current_steps = []
         self.strategy = strategy
         self.num_actions = num_actions
         self.device = device
         self.memory = memory
 
-    def select_action(self, state, policy_net, optimal=False):
+    def select_action(self, state, policy_net, optimal=False, timestep=0):
         if optimal:
             return self.select_optimal_action(state, policy_net)
 
-        rate = self.strategy.get_exploration_rate(self.current_step)
+        if timestep >= len(self.current_steps):
+            self.current_steps = np.append(self.current_steps, 0)
+
+        rate = self.strategy.get_exploration_rate(self.current_steps[timestep])
         if self.memory.can_provide_sample():
-            self.current_step += 1
+            self.current_steps[:timestep] += 1
 
         if rate > random.random():
             action = random.randrange(self.num_actions)
@@ -112,7 +102,7 @@ class Agent:
             return policy_net(state).argmax(dim=1).to(self.device)
 
 
-class EnvManager():
+class EnvManager:
     def __init__(self, environment, device, k=4):
         self.device = device
         self.env = gym.make(environment).unwrapped
@@ -123,7 +113,7 @@ class EnvManager():
 
     def reset(self):
         self.env.reset()
-        self.stack_screens = deque(maxlen=self.k)
+        self.stack_screens = deque(maxlen=4)
 
     def close(self):
         self.env.close()
@@ -135,44 +125,40 @@ class EnvManager():
         return self.env.action_space.n
 
     def take_action(self, action):
-        _, reward, self.done, _ = self.env.step(action.item())
-        rewards = [reward]
+        _, reward, self.done, _ = self.env.step(action)
+        rewards = reward
         # Skip frames and take the same actions for those being skipped (left or right)
         for _ in range(self.k - 1):
-            if action.item() == 1:  # action Fire should just pick Noop
+            if action == 1:  # action Fire should just pick Noop
                 _, reward, self.done, _ = self.env.step(0)
             else:
-                _, reward, self.done, _ = self.env.step(action.item())
-            rewards.append(reward)
+                _, reward, self.done, _ = self.env.step(action)
+            rewards += reward
             if self.done:
                 break
-        return torch.tensor([np.asarray(rewards).sum()])
+        return torch.tensor([rewards])
 
     def just_starting(self):
-        return len(self.stack_screens) < self.k
+        return len(self.stack_screens) < 4
 
     def get_state(self):
-        if self.just_starting() or self.done:
-            # Produce a stack of 4 black screens
+        if self.done:
             self.stack_screens.append(self.get_processed_screen())
-            black_screen = torch.zeros((1, 1, self.get_screen_width(), self.get_screen_height()))
+            black_screen = torch.zeros((1, 4, self.get_screen_width(), self.get_screen_height()))
             return black_screen
         else:
-            # Stack four frames - 4 channel
-            # result_screen = torch.zeros((self.k, self.get_screen_width(), self.get_screen_height()))
-            # for i in range(len(self.stack_screens)):
-            #     result_screen[i] = self.stack_screens[i]
+            if self.just_starting():
+                self.stack_screens.append(self.get_processed_screen())
+                self.stack_screens.append(self.get_processed_screen())
+                self.stack_screens.append(self.get_processed_screen())
+                self.stack_screens.append(self.get_processed_screen())
 
-            #Screen substraction test - 1 channel
-            result = self.stack_screens[0]
-            for i in range(1,len(self.stack_screens)):
-                result -= self.stack_screens[i]
+            result_screen = torch.zeros((4, self.get_screen_width(), self.get_screen_height()))
+            for i in range(len(self.stack_screens)):
+                result_screen[i] = self.stack_screens[i]
 
             self.stack_screens.append(self.get_processed_screen())
-            return result.unsqueeze(0)
-
-            
-            # return result_screen.unsqueeze(0)
+            return result_screen.unsqueeze(0)
 
     def get_processed_screen(self):
         screen = self.render('rgb_array').transpose((2, 0, 1))  # PyTorch expects CHW
